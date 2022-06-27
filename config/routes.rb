a5982f722a85358b8ea831868cf8074a5a2f5dbb
Kyllian Creppy <kyllian.creppy@outlook.fr>
2022-06-27 16:24:44 +0200

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
+  get 'projects', to: 'projects#index'
+  get "projects/:id", to: "projects#show", as: :project
+  get "projects/new", to: "projects#new", as: :new_project
+  get "projects/new", to: "projects#create"
+  get  "projects/:id/edit", to: "projects#edit", as: :edit_project
+  patch "projects/:id", to: "projects#update"
+  delete "projects/new", to: "projects#destroy"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Project.destroy_all
+
+def set_project(name, description)
+    project = Project.new(
+        name: name,
+        description: description,
+    )
+
+    project.save!
+end 
+
+    projects = [
+        {name: "Helpers", description: "Rue des Pyrénées, Paris"},
+        {name: "Valorank", description: "Rue des Pyrénées, Paris"}
+    ]
+
+    projects.each do |project|
+        set_project(project[:name], project [:description])
+    end
